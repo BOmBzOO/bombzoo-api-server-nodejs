5ded0ef377a6865d070cac26a27bb7889ba5726d
BOmBzOO <bombzoo78@gmail.com>
2023-12-20 14:54:35 +0900

    update vercel .env

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import 'dotenv/config';
 import http from 'http';
 import server from './server';
 
-const { PORT } = process.env;
+const { VERCEL_PORT } = process.env;
 
 http.createServer({
 }, server)
-  .listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  .listen(VERCEL_PORT, () => {
+    console.log(`Server is listening on port ${VERCEL_PORT}`);
   });
